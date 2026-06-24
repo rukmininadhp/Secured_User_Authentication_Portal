cb36485a2849f93cc5f63f2c6b308ba95dbfd1df
Rukmini Nadh Pedapudi <heyrukmini@gmail.com>
2026-06-24 18:22:12 +0530

    Add files via upload
    2nd commit

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE p1_ecom;
+
+USE p1_ecom;
+
+CREATE TABLE p1_ecom(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
